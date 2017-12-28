ae4c89cc9ddc46a5e66eb19e38ead2d853957d4f
Dominique Quatravaux <dominique.quatravaux@epfl.ch>
2017-12-28 09:25:13 +0100

    Now with Infoscience mock, and an index page

--- mock-api/index.js
+++ mock-api/index.js
@@ -8,7 +8,11 @@ var servingPort = process.env.MOCK_API_PORT || 3010;
 var app = express()
   .use(json())
   .use(function (req, res) {
-    if (req.url.match(/\/jahia\/memento/)) {
+    console.log(req.url);
+    if (req.url.startsWith("/search?p=")) {
+      res.type("application/xml")
+        .sendFile(__dirname + "/infoscience.xml");
+    } else if (req.url.match(/\/jahia\/memento/)) {
       res.json(mock_memento_fr_jahia);
     } else if (req.url.match(/\/jahia\//)) {
       res.json(mock_actus_fr_jahia);
@@ -17,7 +21,15 @@ var app = express()
       return;
     } else {
       res.status(404).type("text/html")
-        .send("<img src=\"https://upload.wikimedia.org/wikipedia/commons/thumb/2/2f/Peugeot_404_Familiale_1968.jpg/351px-Peugeot_404_Familiale_1968.jpg\">");
+        .send(`<body>
+        <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/2/2f/Peugeot_404_Familiale_1968.jpg/351px-Peugeot_404_Familiale_1968.jpg"></img>
+        <ul>
+              <li><a href="/search?p=infoscience">Infoscience</a></li>
+              <li><a href="/jahia/memento">Memento (Jahia API)</a></li>
+              <li><a href="/jahia/channels">Actu (Jahia API)</a></li>
+              <li><a href="/api/v1/news">Actu (v1 API)</a></li>
+        </ul>
+    </body>`);
     }
   })
   .listen(servingPort, function() {
