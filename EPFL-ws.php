45f4d5b6280526a903d38723da4dc490ee472f6e
Nicolas Borboën <nicolas.borboen@epfl.ch>
2017-12-29 02:22:16 +0100

    Welcome Memento
    * Memento shortcode file added based on `Actu_shortcode.php` - it's a
    work in progress but already working with the default `[memento]`
    shortcode.
    * Reorganisation of the main file of the EPFL-WS plugin: it now uses a
    class and sub-menus renanmed to "_home" instead of "_option".

--- EPFL-ws.php
+++ EPFL-ws.php
@@ -13,43 +13,63 @@ if (! defined('ABSPATH')) {
     die('Access denied.');
 }
 
-add_action( 'admin_menu', 'ws_menu' );
+class EPFLWSmain {
 
-function ws_menu() {
-	add_menu_page( 'EPFL Web Services Options', 'EPFL WS', 'manage_options', 'epfl-ws', 'ws_home', 'dashicons-carrot', 80 );
-	add_submenu_page( 'epfl-ws', 'Actu', 'Actu (news)', 'manage_options', 'epfl-actu', 'actu_options');
-	add_submenu_page( 'epfl-ws', 'Memento', 'Memento (events)', 'manage_options', 'epfl-memento', 'memento_options');
-}
-
-function ws_home() {
-	echo '<div class="wrap">';
-	echo '<h1>EPFL WS</h1>';
-	echo '<p>This is the summary page of the plugin EPFL WS</p>';
-	echo '<p>This Wordpress plugin aim to unify all EPFL web services in one place.</p>';
-	echo '</div>';
-}
-
-function actu_options() {
-	if ( !current_user_can( 'manage_options' ) )  {
-		wp_die( __( 'You do not have sufficient permissions to access this page.' ) );
+	function setup() {
+		add_action( 'admin_menu', array( $this, 'ws_menu') );
 	}
-	echo '<div class="wrap">';
-	echo '  <h1>EPFL Actu</h1>';
-	echo '  <h2>Short code</h2>';
-	echo '  <p>Actu Shortcode allows you to integrate EPFL News (actus) in any Wordpress pages or posts. It uses <a href="https://actu.epfl.ch/api-docs/">https://actu.epfl.ch/api-docs/</a> as an application programming interface (API).</p>';
-	echo '  <p>This documentation is kept short as it will change as soon as the actu API integrates news features.</p>';
-	echo '</div>';
-}
 
-function memento_options() {
-	if ( !current_user_can( 'manage_options' ) )  {
-		wp_die( __( 'You do not have sufficient permissions to access this page.' ) );
+	function ws_menu() {
+		if ( is_admin() ) {
+			add_menu_page( 'EPFL Web Services Options', 'EPFL WS', 'manage_options', 'epfl-ws', array( $this, 'ws_home'), 'dashicons-carrot', 80 );
+			add_submenu_page( 'epfl-ws', 'Actu', 'Actu (news)', 'manage_options', 'epfl-actu', array( $this, 'actu_home'));
+			add_submenu_page( 'epfl-ws', 'Memento', 'Memento (events)', 'manage_options', 'epfl-memento', array( $this, 'memento_home'));
+		}
 	}
-	echo '<div class="wrap">';
-	echo '<p>Here is where the form would go if I actually had options.</p>';
-	echo '</div>';
+
+	function ws_home() {
+		echo '<div class="wrap">';
+		echo '<h1>EPFL WS</h1>';
+		echo '<i>This is the summary page of the plugin EPFL WS</i>';
+		echo '<p>This Wordpress plugin aim to unify all EPFL web services in one place.</p>';
+		echo '<p>Please visit the plugin\'s <a href="https://github.com/epfl-sti/wordpress.plugin.ws">GitHub repository</a> to get latest news, open <a href="https://github.com/epfl-sti/wordpress.plugin.ws/issues">issues</a>, <a href="https://github.com/epfl-sti/wordpress.plugin.ws/pulls">contribute</a> or ask <a href="https://github.com/epfl-sti/wordpress.plugin.ws/issues/new">question</a> to find help.</p>';
+		echo '</div>';
+	}
+
+	function actu_home() {
+		if ( !current_user_can( 'manage_options' ) )  {
+			wp_die( __( 'You do not have sufficient permissions to access this page.' ) );
+		}
+		echo '<div class="wrap">';
+		echo '  <h1>EPFL Actu</h1>';
+		echo '  <h2>Short code</h2>';
+		echo '  <p>The <code>[actu]</code> shortcode allows you to integrate EPFL News (actus) in any Wordpress pages or posts. It uses <a href="https://actu.epfl.ch/api-docs/">https://actu.epfl.ch/api-docs/</a> as an application programming interface (API).</p>';
+		echo '  <p>This documentation is kept short as it will change as soon as the actu API integrates news features.</p>';
+		echo '</div>';
+	}
+
+	function memento_home() {
+		if ( !current_user_can( 'manage_options' ) )  {
+			wp_die( __( 'You do not have sufficient permissions to access this page.' ) );
+		}
+		echo '<div class="wrap">';
+		echo '  <h1>EPFL Memento</h1>';
+		echo '  <h2>Short code</h2>';
+		echo '  <p>The <code>[memento]</code> shortcode allows you to integrate EPFL Events (memento) in any Wordpress pages or posts. It uses <a href="https://memento.epfl.ch/api-docs/">https://memento.epfl.ch/api-docs/ TODO</a> as an application programming interface (API).</p>';
+		echo '  <p>This documentation is kept short as it will change as soon as the memento API integrates news features.</p>';
+		echo '</div>';
+	}
+
 }
 
+// Initialize the plugin.
+$epflwsmain = new EPFLWSmain;
+$epflwsmain->setup();
+
+// "subplugins"
 require_once(dirname(__FILE__) . "/Actu_shortcode.php");
 require_once(dirname(__FILE__) . "/Actu.php");
+require_once(dirname(__FILE__) . "/Memento_shortcode.php");
+
+
 ?>
