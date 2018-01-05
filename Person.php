513fee914a5d2009a61d5ae9ade7fe9ae3403f0d
Dominique Quatravaux <dominique.quatravaux@epfl.ch>
2018-01-05 18:21:05 +0100

    Support for persisting the data in meta boxes
    Use case : the SCIPER of a person
    
    * Plumb down calling save_meta_box_${slug}
    * Add $post parameter to render_meta_box_${slug}

--- Person.php
+++ Person.php
@@ -51,7 +51,10 @@ class PersonConfig
         add_action('init', array(get_called_class(), 'register_post_type'));
 
         /* Customize the edit form */
-        add_action('edit_form_after_title', array(get_called_class(), 'meta_boxes_above_editor'));
+        add_action('edit_form_after_title',
+                   array(get_called_class(), 'meta_boxes_above_editor'));
+        add_action(sprintf('save_post_%s', Person::SLUG),
+                   array(get_called_class(), 'save_meta_boxes'), 10, 3);
     }
 
     /**
@@ -102,6 +105,8 @@ class PersonConfig
      * Add custom fields and behavior to the new person / edit person form
      * using "meta boxes".
      *
+     * Called as the 'register_meta_box_cb' at @link register_post_type time.
+     *
      * @see https://code.tutsplus.com/tutorials/how-to-create-custom-wordpress-writemeta-boxes--wp-20336
      */
     static function add_meta_boxes ()
@@ -113,11 +118,29 @@ class PersonConfig
         }
     }
 
+    static function render_meta_box_find_by_sciper ($unused_post)
+    {
+        ?><input type="text" id="sciper" name="sciper" placeholder="<?php echo ___("SCIPER"); ?>"><?php
+    }
+
+    static function save_meta_box_find_by_sciper ($post_id, $post, $is_update)
+    {
+        update_post_meta($post_id, 'sciper', intval($_REQUEST['sciper']));
+    }
+
+    static function render_meta_box_show_person_details ($post)
+    {
+        $sciper = get_post_meta($post->ID, 'sciper', true);
+        ?><h1>PERSON WITH SCIPER <?php echo $sciper; ?></h1><?php
+    }
+
     /**
      * Simpler version of the WordPress add_meta_box function
      *
      * @param $slug Unique name for this meta box. The render function
-     * is the method called "render_meta_box_$slug"
+     * is the method called "render_meta_box_$slug", and the save function
+     * is the method called "save_meta_box_$slug" (for the latter see
+     * @link save_meta_boxes)
      *
      * @param $title The human-readable title for the meta box
      *
@@ -134,24 +157,49 @@ class PersonConfig
         $meta_box_name = self::_get_meta_box_name($slug);
         add_meta_box($meta_box_name, $title,
                      function () use ($meta_box_name, $klass, $slug) {
-                         wp_nonce_field($meta_box_name);
-                         call_user_func(array($klass, "render_meta_box_$slug"));
+                         wp_nonce_field($meta_box_name, $meta_box_name);
+                         global $post;
+                         call_user_func(
+                             array($klass, "render_meta_box_$slug"),
+                             $post);
                      },
                      null, $position);
     }
 
     private static function _get_meta_box_name ($slug) {
-        return sprintf("%s-meta_box_%s", Person::SLUG, $slug);
+        return sprintf("%s-nonce-meta_box_%s", Person::SLUG, $slug);
     }
 
-    static function render_meta_box_find_by_sciper ()
-    {
-        ?><input type="text" id="sciper" name="sciper" placeholder="<?php echo ___("SCIPER"); ?>"><?php
-    }
+    /**
+     * Call the save_meta_box_$slug for any and all meta box that is
+     * posting information.
+     *
+     * Any and all nonces present in $_REQUEST, for which a corresponding
+     * class method exists, are checked; then the class method is called.
+     */
+    static function save_meta_boxes ($post_id, $post, $is_update) {
+        // Bail if we're doing an auto save
+        if (defined( 'DOING_AUTOSAVE' ) && \DOING_AUTOSAVE) return;
 
-    static function render_meta_box_show_person_details ()
-    {
-        ?><h1>PERSON DETAILS</h1><?php
+        foreach ($_REQUEST as $k => $v) {
+            $matched = array();
+            if (preg_match(sprintf('/%s-nonce-meta_box_([a-zA-Z0-9_]+)$/',
+                                   Person::SLUG),
+                           $k, $matched)) {
+                $save_method_name = "save_meta_box_" . $matched[1];
+                if (method_exists(get_called_class(), $save_method_name)) {
+                    if (! wp_verify_nonce($v, $k)) {
+                        wp_die(___("Nonce check failed"));
+                    } elseif (! current_user_can('edit_post')) {
+                        wp_die(___("Permission denied: edit person"));
+                    } else {
+                        call_user_func(
+                            array(get_called_class(), $save_method_name),
+                            $post_id, $post, $is_update);
+                    }
+                }
+            }
+        }  // End foreach
     }
 
     /**
