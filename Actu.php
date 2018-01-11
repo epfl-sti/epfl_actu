9beb81b1d9b37a0b6c1cb9965cec470f36dca2a1
Dominique Quatravaux <dominique.quatravaux@epfl.ch>
2018-01-11 11:57:45 +0100

    Great Refactoring: base class for ActuController

--- Actu.php
+++ Actu.php
@@ -239,309 +239,46 @@ class ActuStreamController extends \EPFL\WS\Base\APIChannelTaxonomyController
  *
  * This is a "pure static" class; no instances are ever constructed.
  */
-class ActuController
+class ActuController extends \EPFL\WS\Base\APIChannelPostController
 {
-    static function hook ()
+    static function get_taxonomy_class () {
+        return ActuStream::class;
+    }
+
+    static function get_wpadmin_icon () {
+        return 'dashicons-megaphone';
+    }
+
+    static function get_human_labels ()
     {
-        add_action('init', array(get_called_class(), 'register_post_type'));
+        return array(
+            // For register_post_type:
+            'name'               => __x( 'EPFL News', 'post type general name' ),
+            'singular_name'      => __x( 'EPFL News', 'post type singular name' ),
+            'menu_name'          => __x( 'EPFL News', 'admin menu' ),
+            'name_admin_bar'     => __x( 'EPFL News', 'add new on admin bar' ),
+            'view_item'          => ___( 'View EPFL News Item' ),
+            'all_items'          => ___( 'All EPFL News for this site' ),
+            'search_items'       => ___( 'Search News' ),
+            'not_found'          => ___( 'No news found.' ),
+            'not_found_in_trash' => ___( 'No news found in Trash.' ),
 
-        $main_plugin_file = dirname(__FILE__) . "/EPFL-ws.php";
-        register_activation_hook($main_plugin_file, array(get_called_class(), "register_caps"));
-        register_deactivation_hook($main_plugin_file, array(get_called_class(), "deregister_caps"));
-
-        // Behavior of Actu posts on the main site
-        add_filter("post_thumbnail_html",
-                   array(get_called_class(), "filter_post_thumbnail_html"), 10, 5);
-        add_filter("post_type_link",
-                   array(get_called_class(), "filter_post_link"), 10, 4);
-
-        // Behavior of Actu posts in the wp-admin area
-        add_action('admin_init', array(get_called_class(), 'make_subtitles_readonly_in_admin'), 0);
-        add_action( sprintf('manage_%s_posts_columns', Actu::get_post_type()) , array(get_called_class(), "alter_columns"));
-        add_action( sprintf('manage_%s_posts_custom_column', Actu::get_post_type()),
-                    array(get_called_class(), "render_thumbnail_column"), 10, 2);
-        add_action("edit_form_after_title", array(get_called_class(), "render_in_edit_form"));
-        add_action("admin_enqueue_scripts", array(get_called_class(), "editor_css"));
-
-        // Behavior of Actu posts in search results
-        add_filter('pre_get_posts', array(get_called_class(), "pre_get_posts"));
+            // Others:
+            'description'        => ___( 'EPFL News from news.epfl.ch' )
+        );
     }
 
     /**
-     * Make it so that actus pages exist.
-     *
-     * Under WordPress, almost everything publishable is a post.
-     * register_post_type() is invoked to create a particular flavor
-     * of posts that describe news.
+     * Overloaded to show an actual embedded video in the admin area, yow!
      */
-    static function register_post_type ()
+    static function render_thumbnail_column ($epfl_post, $img)
     {
-        register_post_type(
-            Actu::get_post_type(),
-            array(
-                'labels'             => array(
-                    'name'               => __x( 'EPFL News', 'post type general name' ),
-                    'singular_name'      => __x( 'EPFL News', 'post type singular name' ),
-                    'menu_name'          => __x( 'EPFL News', 'admin menu' ),
-                    'name_admin_bar'     => __x( 'EPFL News', 'add new on admin bar' ),
-                    'view_item'          => ___( 'View EPFL News Item' ),
-                    'all_items'          => ___( 'All EPFL News for this site' ),
-                    'search_items'       => ___( 'Search News' ),
-                    'not_found'          => ___( 'No news found.' ),
-                    'not_found_in_trash' => ___( 'No news found in Trash.' )
-                ),
-                'description'        => ___( 'EPFL News from news.epfl.ch' ),
-                'public'             => true,
-                'publicly_queryable' => true,
-                'show_ui'            => true,
-                'show_in_menu'       => true,
-                'query_var'          => true,
-                // ad hoc access control, see (de|)register_caps() below:
-                'capabilities'       => array(
-                    'read'                 => 'read_epfl_actus',
-                    // Name notwithstanding, this is actually the
-                    // permission to see the list of actus.
-                    'edit_posts'           => 'read_epfl_actus',
-                    'create_posts'         => '__NEVER_PERMITTED__',
-
-                    'edit_post'            => 'edit_epfl_actus',
-                    'edit_private_posts'   => 'edit_epfl_actus',
-                    'edit_published_posts' => 'edit_epfl_actus',
-                    'assign_categories'    => 'edit_epfl_actus',
-                    'assign_post_tags'     => 'edit_epfl_actus',
-                    // One is not actually supposed to delete posts manually —
-                    // This is just an escape hatch in case an ActuStream was
-                    // deleted and referential integrity was breached.
-                    'delete_post'          => 'edit_epfl_actus',
-                    'delete_posts'         => 'edit_epfl_actus',
-                    'delete_private_posts' => 'edit_epfl_actus',
-                    'delete_others_posts'  => 'edit_epfl_actus',
-                ),
-                'has_archive'        => true,
-                'hierarchical'       => false,
-                'taxonomies'         => array(ActuStream::get_taxonomy_slug(), 'category'),
-                'menu_position'      => 24,
-                'menu_icon'          => 'dashicons-megaphone',
-                'supports'           => array('wps_subtitle')
-            ));
-    }
-
-    const ROLES_THAT_MAY_VIEW_ACTUS = array('administrator', 'editor', 'author', 'contributor');
-    const ROLES_THAT_MAY_MANAGE_ACTUS = array('administrator', 'editor');
-    const ALL_ROLES = array('administrator', 'editor', 'author', 'contributor', 'subscriber');
-    const CAPS_FOR_VIEWERS = array(
-        'read_epfl_actus',
-    );
-    const ALL_CAPS = array(
-        'read_epfl_actus',
-        'edit_epfl_actus',
-        // Obsolete caps, that still need to be remmoved upon plugin deactivation:
-        'read_epfl_actu',
-        'delete_epfl_actu'
-    );
-
-    /**
-     * Register permissions ("capabilities") on Actu posts.
-     *
-     * Called at plugin activation time.
-     *
-     * The permission map is made so that administrators and editors can view and
-     * delete Actus, but not edit them.
-     */
-    static function register_caps ()
-    {
-        foreach (self::ROLES_THAT_MAY_VIEW_ACTUS as $role_name) {
-            $role = get_role($role_name);
-            foreach (self::CAPS_FOR_VIEWERS as $cap) {
-                $role->add_cap($cap);
-            }
-        }
-        foreach (self::ROLES_THAT_MAY_MANAGE_ACTUS as $role_name) {
-            $role = get_role($role_name);
-            foreach (self::ALL_CAPS as $cap) {
-                $role->add_cap($cap);
-            }
-        }
-    }
-
-    /**
-     * De-register permissions ("capabilities") on Actu posts.
-     *
-     * Called at plugin deactivation time.
-     */
-    static function deregister_caps ()
-    {
-        foreach (self::ALL_ROLES as $role_name) {
-            $role = get_role($role_name);
-            foreach (self::ALL_CAPS as $cap) {
-                $role->remove_cap($cap);
-            }
-        }
-    }
-
-    /**
-     * Alter the columns shown in the Actu list admin page
-     */
-    static function alter_columns ($columns)
-    {
-        // https://stackoverflow.com/a/3354804/435004
-        return array_merge(
-            array_slice($columns, 0, 1, true),
-            array('thumbnail' => ___( 'Thumbnail' )),
-            array_slice($columns, 1, count($columns) - 1, true));
-    }
-
-    static function render_thumbnail_column ($column, $post_id)
-    {
-        if ($column !== 'thumbnail') return;
-        $thumbnail = get_the_post_thumbnail($post_id);
-        if (! $thumbnail) return;
-
-        $actu = Actu::get($post_id);
-        if (! $actu) return;
-
-        if ($actu->get_youtube_id()) {
-            echo '<object style="width:160px;height:89px;float: none; clear: both; margin: 2px auto;" data="https://www.youtube.com/embed/'.$actu->get_youtube_id().'"></object>';
+        if ($epfl_post->get_youtube_id()) {
+            echo '<object style="width:160px;height:89px;float: none; clear: both; margin: 2px auto;" data="https://www.youtube.com/embed/'.$epfl_post->get_youtube_id().'"></object>';
             printf("<p><a href=\"https://youtu.be/%s\">YouTube link</a></p>", $actu->get_youtube_id());
-        } elseif ($orig_size = $actu->get_max_size()) {
-            echo $thumbnail;
-            printf("<p>Size: %dx%d</p>", $orig_size["width"], $orig_size["height"]);
         } else {
-            echo $thumbnail;
+            parent::render_thumbnail_column ($epfl_post);
         }
-
-    }
-
-    /**
-     * Make Actu subtitles read-only by preventing WP Subtitles from
-     * initializing in the case of epfl-actu posts.
-     */
-    static function make_subtitles_readonly_in_admin ()
-    {
-		$post_type = '';
-
-		if ( isset( $_REQUEST['post_type'] ) ) {
-			$post_type = sanitize_text_field( $_REQUEST['post_type'] );
-		} elseif ( isset( $_GET['post'] ) ) {
-			$post_type = get_post_type( absint( $_GET['post'] ) );
-        }
-        if ($post_type !== Actu::get_post_type()) return;
-
-        remove_action('admin_init', array( 'WPSubtitle_Admin', '_admin_init' ) );
-        // Add back the subtitle column:
-        add_filter( 'manage_edit-' . $post_type . '_columns', array( 'WPSubtitle_Admin', 'manage_subtitle_columns' ) );
-        add_action( 'manage_' . $post_type . '_posts_custom_column', array( 'WPSubtitle_Admin', 'manage_subtitle_columns_content' ), 10, 2 );
-    }
-
-    /**
-     * Arrange for get_the_post_thumbnail() to return the external thumbnail for Actus.
-     *
-     * This is set as a filter for WordPress' @link post_thumbnail_html hook. Note that
-     * it isn't as easy to hijack the return value of @link get_the_post_thumbnail_url
-     * in this way (but you can always call the @link get_image_url instance
-     * method on an Actu object).
-     *
-     * @return An <img /> tag with suitable attributes
-     *
-     * @param $orig_html The HTML that WordPress intended to return as
-     *                   the picture (unused, as it will typically be
-     *                   empty — Actu objects lack attachments)
-     *
-     * @param $post_id   The post ID to compute the <img /> for
-     *
-     * @param $size      The requested size, in WordPress notation (either the
-     *                   name of a well-known or declared size, or a [$height,
-     *                   $width] array)
-     *
-     * @param $attr      Associative array of HTML attributes. If "class" is
-     *                   not specified, the default "wp-post-image" is used
-     *                   to match the WordPress behavior for local (attached)
-     *                   images.
-     */
-    static function filter_post_thumbnail_html ($orig_html, $post_id, $unused_thumbnail_id,
-                                                $size, $attr)
-    {
-        $actu = Actu::get($post_id);
-        if (! $actu) return $orig_html;
-
-        // Actu images are resizable server-side
-        // TODO: we could actually interpret $size in a much finer way
-        if (($size === "full" || $size === "large")) {
-            $src = $actu->get_image_url("2048x1152");
-        } else {
-            $src = $actu->get_image_url();
-        }
-        if (! $src) return $orig_html;
-
-        if (! $attr) $attr = array();
-        if (! $attr["class"]) {
-            $attr["class"] = "wp-post-image";
-        }
-        $attrs = "";
-        foreach ( $attr as $name => $value ) {
-            $attrs .= sprintf(" %s=\"%s\"", $name, esc_attr($value));
-        }
-        return sprintf("<img src=\"%s\" %s/>", $src, $attrs);
-    }
-
-    /**
-     * Serve the permalink from actu.epfl.ch instead of our own.
-     *
-     * Mostly, we keep the full text of the article in-database for the search engine.
-     */
-    static function filter_post_link ($orig_link, $post, $unused_leavename, $unused_is_sample)
-    {
-        $actu = Actu::get($post);
-        if (! $actu) return $orig_link;
-        $true_permalink = $actu->get_permalink();
-        return $true_permalink ? $true_permalink : $orig_link;
-    }
-
-    function render_in_edit_form ($wp_post)
-    {
-        $actu = Actu::get($wp_post);
-        if (! $actu) return;
-
-        $permalink = get_permalink($wp_post);
-        global $post;
-        $subtitle = function_exists("get_the_subtitle") ? get_the_subtitle($post, "", "", false) : null;
-        ?>
-    <h1><?php echo $wp_post->post_title; ?></h1>
-    <?php if ($subtitle) : ?><h2><?php echo $subtitle; ?></h2><?php endif; ?>
-	<div id="edit-slug-box" class="hide-if-no-js">
-    <img class="actu-thumbnail" src="<?php echo $actu->get_image_url() ?>"/>
-    <p><b>Permalink:</b> <a href="<?php echo $permalink; ?>"><?php echo $permalink; ?></a></p>
-    <?php echo $wp_post->post_content; ?>
-	</div>
-        <?php
-    }
-
-    static function editor_css ($hook)
-    {
-        if (! ('post.php' === $hook &&
-               Actu::get($_GET["post"])) ) return;
-        wp_register_style(
-            'ws-editor',
-            plugins_url( 'ws-editor.css', __FILE__ ) );
-        wp_enqueue_style('ws-editor');
-    }
-
-    function pre_get_posts ($query) {
-		$qv = &$query->query_vars;
-
-        if (! is_admin() && $query->is_main_query()) {
-            // Loosely based on https://wordpress.stackexchange.com/q/181803/132235
-            $post_types = $query->get('post_type');
-            if ($post_types === 'post') {
-                $post_types = ['post'];
-            }
-            if (is_array($post_types) &&
-                (false === array_search(Actu::get_post_type(), $post_types))) {
-                array_push($post_types, Actu::get_post_type());
-                $query->set('post_type', $post_types);
-            }
-        }
-        return $query;
     }
 }
 
