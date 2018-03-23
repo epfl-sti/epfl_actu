67ac0c206829ebb75efb6b868d469ba22a4ffc66
Dominique Quatravaux <dominique.quatravaux@epfl.ch>
2018-03-23 13:03:29 +0100

    Link to Professor's lab
    * New model method is_lab_owner(), returns the Lab if true

--- Person.php
+++ Person.php
@@ -394,6 +394,23 @@ class Person
         return Lab::get_by_unique_id($unique_id);
     }
 
+    /**
+     * @return a Lab object if this person is the owner of their lab,
+     * false if not, and null if the data is incomplete.
+     */
+    public function is_lab_owner ()
+    {
+        $lab = $this->get_lab();
+        if (! $lab) { return null; }
+        $mgr = $lab->get_lab_manager();
+        if (! $mgr) { return null; }
+        if ($mgr->ID + 0 === $this->ID + 0) {
+            return $lab;
+        } else {
+            return false;
+        }
+    }
+
     public function get_image_url ($size = null)
     {
         return get_post_meta($this->ID, self::THUMBNAIL_META, true);
@@ -772,11 +789,21 @@ class PersonController
         $sciper = $person->get_sciper();
         $title = $person->get_title();
         $greeting = $title ? sprintf("%s ", $title->as_greeting()) : "";
-        $title_str = $title ? sprintf("%s, ", $title->localize()) : "";
-        ?><h1><?php echo $greeting; the_title(); ?></h1>
-          <h2><?php echo $title_str; ?><a href="/epfl-person/<?php echo $sciper; ?>">SCIPER <?php echo $sciper; ?></a> (<a href="https://people.epfl.ch/<?php echo $sciper; ?>">school directory</a>)</h2>
-        <?php the_post_thumbnail(); ?>
-<?php
+        ?><h1><?php echo $greeting; the_title(); ?></h1><?php
+        if ($lab = $person->is_lab_owner()) {
+          printf(
+              '<h2>%s, <a href="/wp-admin/post.php?post=%d&action=edit">%s</a></h2>',
+              $title->localize(),
+              $lab->ID,
+              $lab->get_abbrev()
+          );
+        } else {
+          printf('<h2>%s</h2>', $title_str);
+        }
+        printf(
+            '<h2><a href="/epfl-person/%d">SCIPER %d</a> (<a href="https://people.epfl.ch/%d">school directory</a>)</h2>',
+            $sciper, $sciper, $sciper);
+        the_post_thumbnail();
     }
 
     static function save_meta_box_show_person_details ($post_id, $post, $is_update)
